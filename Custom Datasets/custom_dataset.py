4a1f885bfb9e34b20befe05f1c3a484dcf516499
Ben Russell <benrussell120@hotmail.co.uk>
2025-01-01 19:54:52 +0000

    Updates on custom dataset creation. Half way complete.

--- Custom Datasets/custom_dataset.py
+++ Custom Datasets/custom_dataset.py
@@ -62,7 +62,6 @@ test_dir = image_path / "test"
 
 train_dir, test_dir
 
-
 # Lets visualise an image.
 import random
 from PIL import Image
@@ -98,4 +97,231 @@ img_as_array = np.asarray(img)
 # Plot the image with matplotlib
 plt.figure(figsize = (10, 7))
 plt.imshow(img_as_array)
-plt.title(f"Image class: {image_class} | Image shape": {img_as_array.shape} -> [height, width, color_channels]")
+plt.title(f"Image class: {image_class} | Image shape: {img_as_array.shape} -> [height, width, color_channels]")
+plt.axis(False)
+
+import torch
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
+
+# Write transforms for images
+data_transform = transforms.Compose([
+  # Resize the images to 64X64
+    transforms.Resize(size=(64, 64)),
+    # Flip the images randomely on the horizontal.
+    transforms.RandomHorizontalFlip(p=0.5), # P is the probability of a flip
+    # Turn the image into a tensor.
+    transforms.ToTensor() # This also converts all pixel values from 0 to 255 to be between 0 and 1.
+])
+
+def plot_transformed_images(image_paths, transform, n = 3, seed = 42):
+    """Plots a series of random images from image_paths.
+
+    Will open n image paths from image_paths, transform them
+    with transform and plot them side by side.
+
+    Args:
+        image_paths (list): List of target image paths. 
+        transform (PyTorch Transforms): Transforms to apply to images.
+        n (int, optional): Number of images to plot. Defaults to 3.
+        seed (int, optional): Random seed for the random generator. Defaults to 42.
+    """
+    
+    random.seed(seed)
+    random_image_paths = random.sample(image_paths, k=n)
+    for image_path in random_image_paths:
+        with Image.open(image_path) as f:
+            fig, ax = plt.subplots(1, 2)
+            ax[0].imshow(f) 
+            ax[0].set_title(f"Original \nSize: {f.size}")
+            ax[0].axis("off")
+
+            # Transform and plot image
+            # Note: permute() will change shape of image to suit matplotlib 
+            # (PyTorch default is [C, H, W] but Matplotlib is [H, W, C])
+            transformed_image = transform(f).permute(1, 2, 0) 
+            ax[1].imshow(transformed_image) 
+            ax[1].set_title(f"Transformed \nSize: {transformed_image.shape}")
+            ax[1].axis("off")
+
+            fig.suptitle(f"Class: {image_path.parent.stem}", fontsize=16)
+
+plot_transformed_images(image_path_list, 
+                        transform=data_transform, 
+                        n=3)   
+
+# Use UmageFolder to create dataset(s)
+from torchvision import datasets
+train_data = datasets.ImageFolder(root=train_dir, 
+                                  transform=data_transform, 
+                                  target_transform=None)
+
+test_data = datasets.ImageFolder(root=test_dir,
+                                 transform=data_transform)
+
+print(f"Train data:\n{train_data}\nTest data:\n{test_data}")
+
+class_names = train_data.classes
+class_names
+
+class_dict = train_data.class_to_idx
+class_dict
+
+img, label = train_data[0][0], train_data[0][1]
+print(f"Image tensor:\n{img}")
+print(f"Image shape: {img.shape}")
+print(f"Image datatype: {img.dtype}")
+print(f"Image label: {label}")
+print(f"Label datatype: {type(label)}")
+
+# Rearrange the order of dimensions
+img_permute = img.permute(1, 2, 0)
+
+# Print out different shapes (before and after permute)
+print(f"Original shape: {img.shape} -> [color_channels, height, width]")
+print(f"Image permute shape: {img_permute.shape} -> [height, width, color_channels]")
+
+# Plot the image
+plt.figure(figsize=(10, 7))
+plt.imshow(img.permute(1, 2, 0))
+plt.axis("off")
+plt.title(class_names[label], fontsize=14);
+
+# Turn train and test datasets into dataloaders.
+from torch.utils.data import DataLoader
+train_dataloader = DataLoader(dataset=train_data,
+                              batch_size=1,
+                              num_workers=1,
+                              shuffle=True)
+
+test_dataloader = DataLoader(dataset = test_data, 
+                             batch_size = 1, 
+                             num_workers = 1, 
+                             shuffle = False)
+
+train_dataloader, test_dataloader
+
+img, label = next(iter(train_dataloader))
+
+# Batch size will now be 1.
+print(f"Image shape: {img.shape} -> [batch_size, color_channels, height, width]")
+print(f"Label shape: {label.shape}")
+
+
+import os
+import pathlib
+import torch
+
+from PIL import Image
+from torch.utils.data import Dataset
+from torchvision import transforms
+from typing import Tuple, Dict, List
+
+# Intance of torchvision.datasets.ImageFolder()
+train_data.classes, train_data.class_to_idx
+
+# Setup path for target directory.
+target_directory = train_dir
+
+# Get class names from the target directory.
+class_names_found = sorted([entry.name for entry in list(os.scandir(image_path / "train"))])
+print(f"Class names found: {class_names_found}")
+
+# Make a function to find classes in target directory.
+def find_classes(directory: str) -> Tuple[List[str], Dict[str, int]]:
+    """Finds the class folder names in a target directory.
+    
+    Assumes target directory is in standard image classification format.
+
+    Args:
+        directory (str): target directory to load classnames from.
+
+    Returns:
+        Tuple[List[str], Dict[str, int]]: (list_of_class_names, dict(class_name: idx...))
+    
+    Example:
+        find_classes("food_images/train")
+        >>> (["class_1", "class_2"], {"class_1": 0, ...})
+    """
+    
+    # 1. Get the class names by scanning the target directory
+    classes = sorted(entry.name for entry in os.scandir(directory) if entry.is_dir())
+    
+    # 2. Raise an error if class names not found
+    if not classes:
+        raise FileNotFoundError(f"Couldn't find any classes in {directory}.")
+        
+    # 3. Create a dictionary of index labels (computers prefer numerical rather than string labels)
+    class_to_idx = {cls_name: i for i, cls_name in enumerate(classes)}
+    return classes, class_to_idx
+  
+  
+find_classes(train_dir)
+
+from torch.utils.data import Dataset
+
+# 1. Subclass torch.utils.data.Dataset
+class ImageFolderCustom(Dataset):
+  
+  # 2. Initialise with a targ_dir and transform parameter.
+  def __init__(seld, targ_dir: str, transform = None) -> None:
+    
+    # 3. Create class attributes.
+    # Get all image paths.
+    self.paths = list(pathlib.Path(targ_dir).glob("*/*.jpg"))
+    
+    # Setup transformation
+    self.transform = transform
+    
+    # Create classes and class_to_idx.
+    self.classes, self.class_to_idx = find_classes(targ_dir)
+    
+    
+  # 4. Make function to load images.
+  def load_image(self, index: int) -> Image.Image:
+    "Opens an image cia a path and returns it"
+    image_path = self.paths[index]
+    return Image.open(image_path)
+  
+  # 5. Overwrite the __len__() method
+  def __len__(self) -> int:
+    "Returns the total number of samples"
+    return len(self.paths)
+  
+  # 6. Overwrite the __getitem__() method.
+  def __getitem__(self, index: int) -> Tuple[torch.Tensor, int]:
+    "Returns one sample of data, data and label"
+    img = self.load_image(index)
+    class_name = self.paths[index].parent.name
+    class_idx = self.class_to_idx[class_name]
+    
+    # Transform if necessary
+    if self.transform:
+      return self.transform(img), class_idx
+    else:
+      return img, class_idx
+
+# Augment train data
+train_transforms = transforms.Compose([
+  transforms.Resize((64, 64)),
+  transforms.RandomHorizontalFlip(p=0.5),
+  transforms.ToTensor()
+  
+])
+
+# Don't augment test data, only treshape.
+test_transforms = transforms.Compose([
+  transforms.Resize((64, 64)),
+  transforms.ToTensor()
+])
+
+train_data_custom = ImageFolderCustom(targ_dir = train_dir, 
+                                      transform=train_transforms)
+test_data_custom = ImageFolderCustom(targ_dir=test_dir, 
+                                     transform=test_transforms)
+train_data_custom, test_data_custom
+
+
+
+
+
