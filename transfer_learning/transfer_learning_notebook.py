506c128a151fadfd3286689a33f6c04c8bfdc9a7
Ben Russell <benrussell120@hotmail.co.uk>
2025-01-04 10:25:26 +0000

    Initial.

---
+++ transfer_learning/transfer_learning_notebook.py
@@ -0,0 +1,83 @@
+# For this notebook to run with updated APIs, we need torch 1.12+ and torchvision 0.13+
+try:
+    import torch
+    import torchvision
+    assert int(torch.__version__.split(".")[1]) >= 12, "torch version should be 1.12+"
+    assert int(torchvision.__version__.split(".")[1]) >= 13, "torchvision version should be 0.13+"
+    print(f"torch version: {torch.__version__}")
+    print(f"torchvision version: {torchvision.__version__}")
+except:
+    print(f"[INFO] torch/torchvision versions not as required, installing nightly versions.")
+    !pip3 install -U torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cu113
+    import torch
+    import torchvision
+    print(f"torch version: {torch.__version__}")
+    print(f"torchvision version: {torchvision.__version__}")
+
+# Continue with regular imports
+import matplotlib.pyplot as plt
+import torch
+import torchvision
+
+from torch import nn
+from torchvision import transforms
+
+# Try to get torchinfo, install it if it doesn't work
+try:
+    from torchinfo import summary
+except:
+    print("[INFO] Couldn't find torchinfo... installing it.")
+    !pip install -q torchinfo
+    from torchinfo import summary
+
+# Try to import the going_modular directory, download it from GitHub if it doesn't work
+try:
+    from going_modular.going_modular import data_setup, engine
+except:
+    # Get the going_modular scripts
+    print("[INFO] Couldn't find going_modular scripts... downloading them from GitHub.")
+    !git clone https://github.com/mrdbourke/pytorch-deep-learning
+    !mv pytorch-deep-learning/going_modular .
+    !rm -rf pytorch-deep-learning
+    from going_modular.going_modular import data_setup, engine
+    
+    
+# Setup device agnostic code
+device = "mps"
+
+import os
+import zipfile
+
+from pathlib import Path
+
+import requests
+
+# Setup path to data folder
+data_path = Path("data/")
+image_path = data_path / "pizza_steak_sushi"
+
+# If the image folder doesn't exist, download it and prepare it... 
+if image_path.is_dir():
+    print(f"{image_path} directory exists.")
+else:
+    print(f"Did not find {image_path} directory, creating one...")
+    image_path.mkdir(parents=True, exist_ok=True)
+    
+    # Download pizza, steak, sushi data
+    with open(data_path / "pizza_steak_sushi.zip", "wb") as f:
+        request = requests.get("https://github.com/mrdbourke/pytorch-deep-learning/raw/main/data/pizza_steak_sushi.zip")
+        print("Downloading pizza, steak, sushi data...")
+        f.write(request.content)
+
+    # Unzip pizza, steak, sushi data
+    with zipfile.ZipFile(data_path / "pizza_steak_sushi.zip", "r") as zip_ref:
+        print("Unzipping pizza, steak, sushi data...") 
+        zip_ref.extractall(image_path)
+
+    # Remove .zip file
+    os.remove(data_path / "pizza_steak_sushi.zip")
+    
+# Setup Dirs
+train_dir = image_path / "train"
+test_dir = image_path / "test"
+
